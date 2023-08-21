a59de6f6c33675978f608628837d5f1900fe60e5
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-08-21 10:52:42 -0300

    Exercicio 2 Resolvido

--- _tecinternet_escola_eduardo-resolvido.sql
+++ _tecinternet_escola_eduardo-resolvido.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 21-Ago-2023 às 13:25
+-- Tempo de geração: 21-Ago-2023 às 15:51
 -- Versão do servidor: 10.4.28-MariaDB
 -- versão do PHP: 8.2.4
 
@@ -38,6 +38,21 @@ CREATE TABLE `alunos` (
   `Cursos_id` smallint(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Extraindo dados da tabela `alunos`
+--
+
+INSERT INTO `alunos` (`id`, `nome`, `DataNascimento`, `PrimeiraNota`, `SegundaNota`, `Cursos_id`) VALUES
+(1, 'Alex', '2000-03-21', 9.00, 7.00, 1),
+(2, 'Ben', '2001-05-15', 8.50, 9.00, 2),
+(3, 'Cathy', '1999-11-03', 7.20, 8.00, 3),
+(4, 'David', '2002-02-08', 6.80, 6.50, 4),
+(5, 'Eva', '2003-07-20', 9.50, 9.80, 5),
+(6, 'Frank', '2000-12-10', 8.00, 7.50, 1),
+(7, 'Grace', '2001-09-25', 7.70, 8.70, 2),
+(8, 'Helen', '1998-04-14', 6.00, 5.50, 3),
+(9, 'Ivan', '2002-08-30', 9.20, 9.40, 4);
+
 -- --------------------------------------------------------
 
 --
@@ -48,8 +63,19 @@ CREATE TABLE `cursos` (
   `id` smallint(6) NOT NULL,
   `titulo` varchar(30) NOT NULL,
   `CargaHoraria` smallint(6) NOT NULL,
-  `professores_id` smallint(6) DEFAULT NULL
+  `professores_id` smallint(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `cursos`
+--
+
+INSERT INTO `cursos` (`id`, `titulo`, `CargaHoraria`, `professores_id`) VALUES
+(1, 'Front-End', 40, 5),
+(2, 'Back-End', 80, 4),
+(3, 'UX,UI Design', 30, 2),
+(4, 'Figma', 10, 3),
+(5, 'Redes de computadores', 100, 1);
 
 -- --------------------------------------------------------
 
@@ -60,9 +86,20 @@ CREATE TABLE `cursos` (
 CREATE TABLE `professores` (
   `id` smallint(6) NOT NULL,
   `nome` varchar(50) NOT NULL,
-  `AreaAtuacao` varchar(45) NOT NULL,
+  `AreaAtuacao` enum('Infra','Desenvolvimento','Design','') NOT NULL,
   `Cursos_id` smallint(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `professores`
+--
+
+INSERT INTO `professores` (`id`, `nome`, `AreaAtuacao`, `Cursos_id`) VALUES
+(1, 'Jon oliva', 'Infra', 5),
+(2, 'Lemmy kilmister', 'Design', 4),
+(3, 'Neil peart', 'Design', 3),
+(4, 'Ozzy osbourne', 'Desenvolvimento', 2),
+(5, 'David gilmour', 'Desenvolvimento', 1);
 
 --
 -- Índices para tabelas despejadas
@@ -97,19 +134,19 @@ ALTER TABLE `professores`
 -- AUTO_INCREMENT de tabela `alunos`
 --
 ALTER TABLE `alunos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de tabela `cursos`
 --
 ALTER TABLE `cursos`
-  MODIFY `id` smallint(6) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de tabela `professores`
 --
 ALTER TABLE `professores`
-  MODIFY `id` smallint(6) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Restrições para despejos de tabelas
